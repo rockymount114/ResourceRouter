58a1411e04fcb14961e075d89b5b7c58f8588789
XLi <132082878+rockymount114@users.noreply.github.com>
2024-09-17 08:24:05 -0400

    Delete docs directory

--- docs/RMSData.bat
+++
@@ -1,7 +0,0 @@
-/*  
-  Author:			Shotspotter Mandana Jamei
-  Created Date:			01 OCT 2019
-  Description:			Run Mission Extract SQL Script
-*/ 
-
-sqlcmd -S <sqlservername> -d <databasename> -E -W -h -1 -i "C:\ShotSpotter\filename.sql" -s "," -o "C:\ShotSpotter\DailyUpload_Data.csv"
--- docs/SST_File_Upload_prismatic.bat
+++
@@ -1,1 +0,0 @@
-curl -X POST -H "Content-Type: text/csv" -H "Api-Key: d799bc3ea87b182112eeb787f4c5a876 --data-binary @C:\ShotSpotter\DailyData_Upload.csv https://hooks.prismatic.io/trigger/SW5zdGFuY2VGbG93Q29uZmlnOmIyYzk4MWMyLTEyZDItNDExYS05ZTNiLTc1MGYzNzIzMGJmYg==
